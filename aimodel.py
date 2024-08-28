37279881295f6a45017671e2c12c16accb84a7bc
Pymaster3119 <adi31tya@icloud.com>
2024-08-28 12:51:40 -0400

    Making learning better

--- aimodel.py
+++ aimodel.py
@@ -35,13 +35,13 @@ class Agent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=2000)
+        self.memory = deque(maxlen=100000)
         self.gamma = 0.95    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.9995
-        self.learning_rate = 0.001
-        self.batch_size = 100
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.0001
+        self.batch_size = 256
         self.train_start = 1000
         self.device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
         self.model = DQN(state_size, action_size).to(self.device)
